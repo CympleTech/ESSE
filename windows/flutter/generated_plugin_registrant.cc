c18b127d5716872b57599674be15b40f65ea887e
Sun <huachuang20@gmail.com>
2021-08-24 10:52:37 +0800

    upgrade deps

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -8,13 +8,13 @@
 
 #include <esse_core/esse_core_plugin.h>
 #include <file_selector_windows/file_selector_plugin.h>
-#include <url_launcher_windows/url_launcher_plugin.h>
+#include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   EsseCorePluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("EsseCorePlugin"));
   FileSelectorPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSelectorPlugin"));
-  UrlLauncherPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("UrlLauncherPlugin"));
+  UrlLauncherWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
